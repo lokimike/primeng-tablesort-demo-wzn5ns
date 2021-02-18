e72fe5d8a7eaeeb9b23b4c04e697b14db0bebce4
lokimike <lokmike626@gmail.com>
2021-02-19 02:27:04 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,51 @@
-import {Component} from '@angular/core';
-import { ProductService } from './productservice';
-import { Product } from './product';
-import { SortEvent } from 'primeng/api';
+import { Component } from "@angular/core";
+import { ProductService } from "./productservice";
+import { Product } from "./product";
+import { SortEvent } from "primeng/api";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
-export class AppComponent { 
-    products1: Product[];
+export class AppComponent {
+  products1;
 
-    products2: Product[];
+  products2: Product[];
 
-    products3: Product[];
+  products3: Product[];
 
-    constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private http: HttpClient
+  ) {}
 
-    ngOnInit() {
-        this.productService.getProductsSmall().then(data => this.products1 = data);
-        this.productService.getProductsSmall().then(data => this.products2 = data);
-        this.productService.getProductsSmall().then(data => this.products3 = data);
-    }
+  ngOnInit() {
+    this.http
+      .get<any>("assets/products-small.json")
+      .toPromise()
+      .then(res => <any>res.data)
+      .then(data => {
+        this.products1 = data;
+        
+      });
+  }
 
-    customSort(event: SortEvent) {
-        event.data.sort((data1, data2) => {
-            let value1 = data1[event.field];
-            let value2 = data2[event.field];
-            let result = null;
+  customSort(event: SortEvent) {
+    event.data.sort((data1, data2) => {
+      let value1 = data1[event.field];
+      let value2 = data2[event.field];
+      let result = null;
 
-            if (value1 == null && value2 != null)
-                result = -1;
-            else if (value1 != null && value2 == null)
-                result = 1;
-            else if (value1 == null && value2 == null)
-                result = 0;
-            else if (typeof value1 === 'string' && typeof value2 === 'string')
-                result = value1.localeCompare(value2);
-            else
-                result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+      if (value1 == null && value2 != null) result = -1;
+      else if (value1 != null && value2 == null) result = 1;
+      else if (value1 == null && value2 == null) result = 0;
+      else if (typeof value1 === "string" && typeof value2 === "string")
+        result = value1.localeCompare(value2);
+      else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
 
-            return (event.order * result);
-        });
-    }
+      return event.order * result;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent }   from './app.component';
-import { ProductService } from './productservice';
-import {DropdownModule} from 'primeng/dropdown';
-import {TableModule} from 'primeng/table';
-import {ButtonModule} from 'primeng/button';
+import { AppComponent } from "./app.component";
+import { ProductService } from "./productservice";
+import { DropdownModule } from "primeng/dropdown";
+import { TableModule } from "primeng/table";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
   imports: [
@@ -20,9 +20,8 @@ import {ButtonModule} from 'primeng/button';
     HttpClientModule,
     FormsModule
   ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   providers: [ProductService]
 })
-
-export class AppModule { }
+export class AppModule {}
