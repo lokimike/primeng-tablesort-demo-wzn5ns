1f70db61bfee5c9da27bd649321aee7516bac080
lokimike <lokmike626@gmail.com>
2021-02-19 19:08:52 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectorRef, ngOnInit } from "@angular/core";
 import { ProductService } from "./productservice";
 import { Product } from "./product";
 import { SortEvent } from "primeng/api";
@@ -16,36 +16,46 @@ export class AppComponent {
 
   products3: Product[];
 
+  branchType;
+  selectedType;
   constructor(
     private productService: ProductService,
-    private http: HttpClient
-  ) {}
+    private http: HttpClient,
+    private cd: ChangeDetectorRef
+  ) {
+    this.branchType = [
+      { name: "Branch", code: "branch" },
+      { name: "Location", code: "location" }
+    ];
+  }
 
   ngOnInit() {
     this.http
       .get<any>("assets/products-small.json")
       .toPromise()
-      .then(res => <any>res.data)
+      .then(res => <any>res.branch)
       .then(data => {
         this.products1 = data;
-        
       });
   }
 
-  customSort(event: SortEvent) {
-    event.data.sort((data1, data2) => {
-      let value1 = data1[event.field];
-      let value2 = data2[event.field];
-      let result = null;
-
-      if (value1 == null && value2 != null) result = -1;
-      else if (value1 != null && value2 == null) result = 1;
-      else if (value1 == null && value2 == null) result = 0;
-      else if (typeof value1 === "string" && typeof value2 === "string")
-        result = value1.localeCompare(value2);
-      else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
-
-      return event.order * result;
-    });
+  OnDropChange(option) {
+    if (option == "location") {
+      this.http
+        .get<any>("assets/products-small.json")
+        .toPromise()
+        .then(res => <any>res.location)
+        .then(data => {
+          this.products1 = data;
+        });
+    } else {
+      this.http
+        .get<any>("assets/products-small.json")
+        .toPromise()
+        .then(res => <any>res.branch)
+        .then(data => {
+          this.products1 = data;
+        });
+    }
   }
 }
