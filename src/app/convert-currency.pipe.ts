d873be8a8f9be6859efcc995348de3e8c9832687
lokimike <lokmike626@gmail.com>
2021-02-21 22:59:58 +0530

    2 files modified

--- src/app/convert-currency.pipe.ts
+++ src/app/convert-currency.pipe.ts
@@ -5,6 +5,20 @@ import { DecimalPipe } from "@angular/common";
 export class ConvertCurrencyPipe implements PipeTransform {
   constructor(private decimalPipe: DecimalPipe) {}
   transform(value: any, digits?: any): any {
-    return this.decimalPipe.transform(value / 1000000, digits);
+    if (value <= 999) {
+      return value;
+    }
+    // thousands
+    else if (value >= 1000 && value <= 999999) {
+      return this.decimalPipe.transform(value / 1000, digits) + "K";
+    }
+    // millions
+    else if (value >= 1000000 && value <= 999999999) {
+      return this.decimalPipe.transform(value / 1000000, digits) + "M";
+    }
+    // billions
+    else if (value >= 1000000000 && value <= 999999999999) {
+      return this.decimalPipe.transform(value / 1000000000, digits) + "B";
+    } else return value;
   }
 }
